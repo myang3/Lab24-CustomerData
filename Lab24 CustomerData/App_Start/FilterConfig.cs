af495ed1eba10b055df266eecaf73a4342fce79d
moyang9192 <moyang9192@gmail.com>
2016-09-28 13:47:37 -0400

    Add project files.

---
+++ Lab24 CustomerData/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Lab24_CustomerData
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Lab24 CustomerData/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab24_CustomerData.MvcApplication" Language="C#" %>
